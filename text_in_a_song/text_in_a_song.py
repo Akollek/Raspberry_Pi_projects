dc227be15cacd14bc0cf47316130a7300539027a
Akollek <pi@raspberrypi.(none)>
2014-06-02 23:08:40 +0000

    songs with special chars in name now play

--- text_in_a_song/text_in_a_song.py
+++ text_in_a_song/text_in_a_song.py
@@ -10,13 +10,13 @@ def HandleSongRequest():
 #	Search for the song
 	results=search.youtube_search(request.values.get('Body', None))
 	id=results[0]
-	title=results[1]	
+	title=results[1]
+	filename=id+".m4a"	
 
 #	Download the song
-	call(['youtube-dl','-x',id])
+	call(['youtube-dl','-o',filename,'-x',id])
 			
 #	Play the song
-	filename=title+'-'+id+'.m4a'
 	call(['omxplayer',filename])
 	
 #	Delete the file
