e6eb7dcb0ff5097767df144cfe145df081fcf32f
Akollek <amiel.kollek@mail.mcgill.ca>
2014-07-26 18:29:35 +0000

    Cleaned up how it looks in terminal, fixed song reporting

--- 8tracks/8tracks.sh
+++ 8tracks/8tracks.sh
@@ -1,9 +1,11 @@
 
 #!/bin/bash
 
+clear
+
 # Find the mix ID
 MIX=$1
-curl "http://8tracks.com/$MIX.json?api_key=2256b5ab4f6f92d858c9530bd68900e0d0ff42f6" > mix.json 
+curl -s "http://8tracks.com/$MIX.json?api_key=2256b5ab4f6f92d858c9530bd68900e0d0ff42f6" > mix.json 
 python mix_parser.py mix
 MIXID=`cat mix`
 LENGTH=`cat tracks`
@@ -12,14 +14,14 @@ rm mix
 rm mix.json
 
 #Create a play token
-curl "http://8tracks.com/sets/new.json?api_key=2256b5ab4f6f92d858c9530bd68900e0d0ff42f6" > token.json 
+curl -s "http://8tracks.com/sets/new.json?api_key=2256b5ab4f6f92d858c9530bd68900e0d0ff42f6" > token.json 
 python token_parser.py
 TOKEN=`cat token`
 rm token
 rm token.json
 
 # Get the first song
-curl "http://8tracks.com/sets/$TOKEN/play.json?mix_id=$MIXID&api_key=2256b5ab4f6f92d858c9530bd68900e0d0ff42f6" > song.json 
+curl -s "http://8tracks.com/sets/$TOKEN/play.json?mix_id=$MIXID&api_key=2256b5ab4f6f92d858c9530bd68900e0d0ff42f6" > song.json 
 python song_parser.py
 URL=`cat song`
 TITLE=`cat title`
@@ -41,11 +43,11 @@ echo "==========================================================================
 echo
 
 # Report that the song was played
-#curl "http://8tracks.com/sets/874076615/report.xml?track_id=$TRACK&mix_id=$MIXID&api_key=2256b5ab4f6f92d858c9530bd68900e0d0ff42f6" &
+curl -s "http://8tracks.com/sets/$TOKEN/report.json?track_id=$TRACK&mix_id=$MIXID&api_key=2256b5ab4f6f92d858c9530bd68900e0d0ff42f6&api_version=3" >/dev/null &
 
 for (( c=2; c<=$LENGTH; c++ ))
 do
-curl "http://8tracks.com/sets/$TOKEN/next.json?mix_id=$MIXID&api_key=2256b5ab4f6f92d858c9530bd68900e0d0ff42f6" > song.json 
+curl -s "http://8tracks.com/sets/$TOKEN/next.json?mix_id=$MIXID&api_key=2256b5ab4f6f92d858c9530bd68900e0d0ff42f6" > song.json 
 python song_parser.py
 URL=`cat song`
 TITLE=`cat title`
@@ -65,7 +67,7 @@ echo "==========================================================================
 echo
 
 # Report that the song was played
-#curl "http://8tracks.com/sets/874076615/report.xml?track_id=$TRACK&mix_id=$MIXID&api_key=2256b5ab4f6f92d858c9530bd68900e0d0ff42f6" &
+curl -s "http://8tracks.com/sets/$TOKEN/report.json?track_id=$TRACK&mix_id=$MIXID&api_key=2256b5ab4f6f92d858c9530bd68900e0d0ff42f6&api_version=3" >/dev/null &
 done
 
 
@@ -84,7 +86,7 @@ echo "Play a similar playlists? (yes=1/no=2)"
 read NEXT
 
 if [ "$NEXT" == 1 ]; then
-	curl "http://8tracks.com/sets/111696185/next_mix.json?mix_id=$MIXID&api_key=2256b5ab4f6f92d858c9530bd68900e0d0ff42f6" > next.json
+	curl -s "http://8tracks.com/sets/111696185/next_mix.json?mix_id=$MIXID&api_key=2256b5ab4f6f92d858c9530bd68900e0d0ff42f6" > next.json
 	python next_mix_parser.py
 	NEW=`cat next_mix`
 	rm next_mix
